da2a0e820fa84d1d32fdf55e9d33ad6d44e509e5
Mirza <mirza@Mirzas-MBP.lan>
2019-03-04 21:25:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
 
 class App extends Component {
+  state = {
+    tags: ["Flower", "Bird", "Cat"]
+  };
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main">
+        <h1>Outputing Some Data</h1>
+        <ul>{this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}</ul>
       </div>
     );
   }
